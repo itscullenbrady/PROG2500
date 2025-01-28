77db2484bbb28035268c1512e27f9593e296dc2d
Ize <cullenbrady6698@gmail.com>
2025-01-28 14:25:57 -0400

    minor changes for A3

--- PROG2500_A3/PROG2500_A3/obj/Debug/net9.0-windows/PROG2500_A3.AssemblyInfo.cs
+++ PROG2500_A3/PROG2500_A3/obj/Debug/net9.0-windows/PROG2500_A3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROG2500_A3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a628d923f32cc84064a888e62e98fede15f5d2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROG2500_A3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROG2500_A3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
